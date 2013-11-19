9f8c34c07d66f3d872dbac4420f986dca2973992
Willian van der Velde <willian@pythonheads.nl>
2013-11-19 20:29:02 +0100

    Oopsie, tests in wrong folder

---
+++ spec/models/spree/product_property_decorator_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Spree::ProductProperty do
+  context "After save" do
+    let(:product_property) { FactoryGirl.create :product_property }
+    
+    it "should update the product data hstore" do
+      product_property.product.should_receive(:data=).with({})
+      product_property.product.should_receive(:save!).once
+      product_property.save!
+    end
+  end
+end
