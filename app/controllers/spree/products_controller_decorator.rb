4dcc066073fd6342a799633b11ff440c8a8fa868
Willian van der Velde <willian@pythonheads.nl>
2013-11-27 13:45:56 +0100

    Using properties marked as filterable

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -2,7 +2,6 @@ Spree::ProductsController.class_eval do
   
   def index_with_filter
     index_without_filter
-    filterables = %w"baseball_cap_color" # todo: this shouldnt be here
     filter_on = filterables.select { |key| params.has_key? key }
     filter_on.each do |filterable|
       # todo: use here the faster Person.where("data @> 'foo=>bar'") syntax
@@ -12,4 +11,9 @@ Spree::ProductsController.class_eval do
 
   alias_method :index_without_filter, :index
   alias_method :index, :index_with_filter
+
+  private
+  def filterables
+    Spree::Property.where(:filterable => true).pluck(:name)
+  end
 end
--- spec/controllers/spree/product_controller_decorator_spec.rb
+++ spec/controllers/spree/product_controller_decorator_spec.rb
@@ -1,11 +1,22 @@
 require 'spec_helper'
 
 describe Spree::ProductsController do
-  let!(:product_with_the_blue_cap) { 
+  let!(:product_property_with_the_blue_cap) { 
     product_property = FactoryGirl.create :product_property 
     product_property.value = 'blue'
     product_property.save!
-    product_property.product
+    product_property
+  }
+
+  let!(:product_with_the_blue_cap) { 
+    product_property_with_the_blue_cap.product
+  }
+
+  let!(:filterable_property) {
+    property = product_property_with_the_blue_cap.property
+    property.filterable = true
+    property.save!
+    property
   }
 
   let!(:product_with_the_red_cap) { 
@@ -15,14 +26,25 @@ describe Spree::ProductsController do
     product_property.product
   }
 
-  it 'should filter on the data params' do
-    spree_get :index, baseball_cap_color: 'blue'
-    expect(assigns[:products]).to eq([product_with_the_blue_cap])
+
+  describe '.filterables' do
+    it 'should only return properties marked as filterable' do
+      filterables = Spree::ProductsController.new.send(:filterables)
+      expect(filterables).to eq([filterable_property.name,])
+    end
   end
 
-  it 'should not filter if no params are given' do
-    spree_get :index
-    expect(assigns[:products]).to eq([product_with_the_red_cap, product_with_the_blue_cap])
-  end
+  describe '.index' do
+    describe 'filter' do
+      it 'should filter on the data params' do
+        spree_get :index, baseball_cap_color: 'blue'
+        expect(assigns[:products]).to eq([product_with_the_blue_cap])
+      end
 
+      it 'should not filter if no params are given' do
+        spree_get :index
+        expect(assigns[:products]).to eq([product_with_the_red_cap, product_with_the_blue_cap])
+      end
+    end
+  end
 end
