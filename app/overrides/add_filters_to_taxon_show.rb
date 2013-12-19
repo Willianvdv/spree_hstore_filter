8e50934cad83465cb252b00fcb3fe08e1ff63fb0
Willian van der Velde <willian@pythonheads.nl>
2013-12-19 16:59:09 +0100

    Bind on taxon_sidebar_navigation

--- app/overrides/add_filters_to_taxon_show.rb
+++ app/overrides/add_filters_to_taxon_show.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/shared/_filters",
                      :name => "add_filters_to_taxon_show",
-                     :insert_top => '[data-hook="navigation"]',
+                     :insert_top => '[data-hook="taxon_sidebar_navigation"]',
                      :partial => "spree/taxons/filters")
