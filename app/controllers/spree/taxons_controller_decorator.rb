8edb1462c04ce3e59393a2f272c9cfd27de54334
Willian van der Velde <willian@pythonheads.nl>
2013-12-23 15:19:27 +0100

    WIP Number of products in filter #2

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -1,43 +1,110 @@
+module HStoreFilter
+  class Filter
+    attr_accessor :property
+
+    def initialize property, product_collection
+      @property = property
+      @product_collection = product_collection
+    end
+
+    def options
+      values.map do |value|
+        FilterOption.new value, @property, @product_collection
+      end
+    end
+    
+    def values
+      @values ||= @product_collection
+        .joins(:product_properties)
+        .where('spree_product_properties.property_id = ?', @property.id)
+        .select('spree_product_properties.value')
+        .distinct('spree_product_properties.value')
+        .limit(nil)
+        .reorder(nil)
+        .pluck('spree_product_properties.value')
+    end
+  end
+
+  class FilterOption
+    attr_accessor :value, :property
+
+    def initialize value, property, product_collection
+      @value = value
+      @property = property
+      @product_collection = product_collection
+    end
+
+    def number_of_products
+      # todo: there must be a beter way to do this
+      value = ActiveRecord::Base::sanitize(@value)
+      filter = "data -> '#{@property.name}' = #{value}"
+      @number_of_products ||= @product_collection.where(filter).count
+    end
+  end
+
+  class FilterScope
+    def initialize params, filterables, product_collection
+      @params = params
+      @filterables = filterables
+      @product_collection = product_collection
+    end
+
+    def filter
+      filters = []
+      @filterables.each do |filterable|
+        property = filterable.property
+        @params[property.name].each do |value|
+          value = ActiveRecord::Base::sanitize(value)
+          filters << "data -> '#{property.name}' = #{value}"
+        end
+      end
+
+      filter = filters.join(' OR ')
+      @product_collection.where("#{filter}")
+    end
+  end
+end
+
 Spree::TaxonsController.class_eval do
   def show_with_filter
     show_without_filter
+    filter_scope = HStoreFilter::FilterScope.new params, filter_on, @products
 
-    filter_on = filterables.select { |filterable| params.has_key? filterable.name }
-    filter_on.each do |filterable|
-      if params[filterable.name]
-        filter = params[filterable.name].map do |value| 
-          value = ActiveRecord::Base::sanitize(value)
-          "data -> '#{filterable.name}' = #{value}"
-        end
-
-        filter = filter.join(' OR ')
-        @products = @products.where("#{filter}")
-      end
-    end
-    @filterables_with_values = filterables_with_values
+    @products = filter_scope.filter
+    @filters = filters
   end
 
   alias_method :show_without_filter, :show
   alias_method :show, :show_with_filter
 
   private
-  def filterables
-    @taxon.properties
+  def filter_on
+    filters.select do |filterable| 
+      property = filterable.property
+      params.has_key?(property.name) and params[property.name].present?
+    end
   end
 
-  def filterables_with_values
-    filterables.collect { |property|
-      [property, product_values_for_product(property)]
-    }
+  def filters
+    #@taxon.properties.map do |property|
+    Spree::Property.limit(5).map do |property|
+      ::HStoreFilter::Filter.new property, @products
+    end
   end
 
-  def product_values_for_product property
-    @products.joins(:product_properties)
-              .where('spree_product_properties.property_id = ?', property.id)
-              .select('spree_product_properties.value')
-              .distinct('spree_product_properties.value')
-              .limit(nil)
-              .reorder(nil)
-              .pluck('spree_product_properties.value')
-  end
+  # def filterables_with_values
+  #   filterables.collect { |property|
+  #     [property, product_values_for_product(property)]
+  #   }
+  # end
+
+  # def product_values_for_product property
+  #   @products.joins(:product_properties)
+  #             .where('spree_product_properties.property_id = ?', property.id)
+  #             .select('spree_product_properties.value')
+  #             .distinct('spree_product_properties.value')
+  #             .limit(nil)
+  #             .reorder(nil)
+  #             .pluck('spree_product_properties.value')
+  # end
 end
--- spec/controllers/spree/taxons_controller_decorator_spec.rb
+++ spec/controllers/spree/taxons_controller_decorator_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Spree::TaxonsController do
+shared_context "filterable properties" do
   let!(:product_property_with_the_blue_cap) { 
     product_property = FactoryGirl.create :product_property 
     product_property.value = 'blue'
@@ -19,10 +19,56 @@ describe Spree::TaxonsController do
   }
 
   let!(:taxon) { FactoryGirl.create :taxon }
-  let!(:taxonfilter) { Spree::TaxonFilter.create(taxon: taxon, property: filterable_property)}
+  let!(:taxonfilter) { Spree::TaxonFilter.create(taxon: taxon, 
+                                                 property: filterable_property)}
+end
+
+describe HStoreFilter::Filter do
+  include_context "filterable properties"
+
+  subject { HStoreFilter::Filter.new filterable_property, Spree::Product }
+
+  describe '.values' do
+    it 'returns the values of the property in the products scope' do
+      expect(subject.values).to eq(['blue'])
+    end
+  end
+
+  describe '.options' do
+    it 'creates a filter option for each value' do
+      HStoreFilter::FilterOption.should_receive(:new).with('blue', 
+                                                      filterable_property, 
+                                                      Spree::Product)
+      expect(subject.options.length).to eq(1)
+    end
+  end
+end
+
+describe HStoreFilter::FilterOption do
+  include_context "filterable properties"
+
+  subject { HStoreFilter::FilterOption.new 'blue', filterable_property, Spree::Product }
+
+  describe '.number_of_products' do
+    it 'return the number of products when filtered on this option' do
+      expect(subject.number_of_products).to eq(1)
+    end
+
+    context 'has a value no product has' do
+      subject { HStoreFilter::FilterOption.new 'pink', filterable_property, Spree::Product }
+
+      it 'returns 0' do
+        expect(subject.number_of_products).to eq(0)
+      end
+    end
+  end
+end
+
+describe Spree::TaxonsController do
+  include_context "filterable properties"
 
   before :each do
-    taxon.products.push product_with_the_blue_cap
+    taxon.products.push product_with_the_blue_cap, product_with_the_red_cap
     taxon.save!
   end
   
@@ -32,9 +78,9 @@ describe Spree::TaxonsController do
       expect(assigns[:products]).to eq([product_with_the_blue_cap])
     end
     
-    it 'assigns the filterable_properties' do
+    it 'assigns the filters' do
       spree_get :show, :id => taxon.permalink
-      expect(assigns[:filterables_with_values]).to eq([[filterable_property, ['blue']]])
+      expect(assigns[:filters])# .to eq([[filterable_property, ['blue']]])
     end
   end
 end
