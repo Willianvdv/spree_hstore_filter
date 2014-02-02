91a246b9539bd133fb5e987bc5d46380ba6b27c9
Willian van der Velde <willian@pythonheads.nl>
2014-02-02 19:27:59 +0100

    Moved filters to lib

---
+++ lib/h_store_filter/filter.rb
@@ -0,0 +1,27 @@
+module HStoreFilter
+  class Filter
+    attr_accessor :property
+
+    def initialize property, product_collection
+      @property = property
+      @product_collection = product_collection
+    end
+
+    def options
+      values.map do |value|
+        FilterOption.new value, @property, @product_collection
+      end
+    end
+    
+    def values
+      @values ||= @product_collection
+        .joins(:product_properties)
+        .where('spree_product_properties.property_id = ?', @property.id)
+        .select('spree_product_properties.value')
+        .distinct('spree_product_properties.value')
+        .limit(nil)
+        .reorder(nil)
+        .pluck('spree_product_properties.value')
+    end
+  end
+end
---
+++ lib/h_store_filter/filter_option.rb
@@ -0,0 +1,20 @@
+module HStoreFilter
+  class FilterOption
+    attr_accessor :value, :property
+
+    def initialize value, property, product_collection
+      @value = value
+      @property = property
+      @product_collection = product_collection
+    end
+
+    def number_of_products
+      hstore_filter = ActiveRecord::Base::sanitize "#{@property.name}=>\"#{@value}\""
+      filter = "data @> #{hstore_filter}::hstore"
+      @number_of_products ||= @product_collection
+                                  .limit(nil)
+                                  .where(filter)
+                                  .count
+    end
+  end
+end
