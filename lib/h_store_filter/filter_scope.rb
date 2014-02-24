46c720c83cd3321eb37adf953a9a621b3ddcb419
Willian van der Velde <willian@pythonheads.nl>
2014-02-24 13:24:09 +0100

    Space in name fix

---
+++ lib/h_store_filter/filter_scope.rb
@@ -0,0 +1,26 @@
+module HStoreFilter
+  class FilterScope
+    def initialize(params, filterables, product_collection)
+      @params = params
+      @filterables = filterables
+      @product_collection = product_collection
+    end
+
+    def filter
+      filters = []
+      @filterables.each do |filterable|
+        filters_per_filterable = []
+        property = filterable.property
+        @params[property.name].each do |value|
+          hstore_filter = ActiveRecord::Base.sanitize "\"#{property.name}\"=>\"#{value}\""
+          filters_per_filterable << "data @> #{hstore_filter}::hstore"
+        end
+
+        filters << filters_per_filterable.join(' OR ')
+      end
+
+      filter = filters.join(') AND (')
+      filters.empty? ? @product_collection : @product_collection.where("(#{filter})")
+    end
+  end
+end
