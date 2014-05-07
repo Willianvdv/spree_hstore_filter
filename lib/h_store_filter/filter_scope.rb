4ea06a32f0b77dcca7a8e36bbfd749a7597e5a7e
Willian van der Velde <willian@pythonheads.nl>
2014-05-07 14:57:47 +0200

    Single value

--- lib/h_store_filter/filter_scope.rb
+++ lib/h_store_filter/filter_scope.rb
@@ -11,7 +11,11 @@ module HStoreFilter
       @filterables.each do |filterable|
         filters_per_filterable = []
         property = filterable.property
-        @params[property.name].each do |value|
+
+        values = @params[property.name]
+        values = [values] unless values.is_a? Array
+
+        values.each do |value|
           hstore_filter = ActiveRecord::Base.sanitize "\"#{property.name}\"=>\"#{value}\""
           filters_per_filterable << "data @> #{hstore_filter}::hstore"
         end
